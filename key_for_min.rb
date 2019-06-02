832fbceadac74317fc6466700bae0716d7cb3d1b
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 18:30:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if my_value == nil || my_value < value
       my_key = key
-      puts "Hello!"
+      puts my_value
     end
   end
   my_key
